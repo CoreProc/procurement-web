53d1562ee5a94f13d3e3a52d60ae080ffa812ff9
awkwardusername <awkwardusername@gmail.com>
2014-11-21 15:48:58 +0800

    updated gitignore, set DB to match environment settings

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'kitchen',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'procex',
+			'username'  => getenv('DB_USER'),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

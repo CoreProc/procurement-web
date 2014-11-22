556bc7ffb715eca9d6ae6c432b66bcb5701ae451
awkwardusername <awkwardusername@gmail.com>
2014-11-23 06:13:15 +0800

    enable CORS

--- app/config/app.php
+++ app/config/app.php
@@ -125,6 +125,7 @@ return array(
         'Toin0u\Geotools\GeotoolsServiceProvider',
         'EllipseSynergie\ApiResponse\Laravel\ResponseServiceProvider',
         'Barryvdh\HttpCache\ServiceProvider',
+        'Barryvdh\Cors\CorsServiceProvider',
     ),
 
     /*

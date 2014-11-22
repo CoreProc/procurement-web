c1496d0e1ba120b217b1e44b18cc3965327429a4
Chris Bautista <chrisbjr@gmail.com>
2014-11-22 18:50:39 +0800

    Shortened HELP message

--- app/lang/en/procex.php
+++ app/lang/en/procex.php
@@ -4,7 +4,7 @@ return [
 
     'smsMessages' => [
 
-        'help' => 'Procurement Explorer keywords are:\n- INQUIRE - know how many projects are being procured in your current location\n- HELP - Get a list of keywords to help you with this service',
+        'help' => 'Procurement Explorer keywords are:\n- INQUIRE - summary of projects your current location\n- HELP - Get a list of keywords to help you with this service',
 
     ],
 

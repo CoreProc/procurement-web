ce76bbb833558166362867fad8e36b6932a08163
awkwardusername <awkwardusername@gmail.com>
2014-11-22 23:34:43 +0800

    revert model

--- app/procex/Model/BidInformation.php
+++ app/procex/Model/BidInformation.php
@@ -16,7 +16,7 @@ class BidInformation extends ProcurementModel
 
     public function awards()
     {
-        return $this->hasMany('Coreproc\Procex\Model\Award', 'ref_no', 'ref_id');
+        return $this->hasMany('Coreproc\Procex\Model\Award', 'ref_id');
     }
 
     public function procuringEntity()
